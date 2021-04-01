d08c5f898d1c4d93cd954544770ee10c1f4a1148
alegazzo <72534448+alegazzo@users.noreply.github.com>
2021-03-31 21:37:47 -0300

    commit ej 4

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.buttonMover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.CheckOnClick = true;
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Items.AddRange(new object[] {
+            "Uno",
+            "Dos",
+            "Tres",
+            "Cuatro",
+            "Cinco",
+            "Seis",
+            "Siete",
+            "Ocho",
+            "Nueve"});
+            this.checkedListBox.Location = new System.Drawing.Point(26, 46);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(299, 349);
+            this.checkedListBox.TabIndex = 0;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Items.AddRange(new object[] {
+            " "});
+            this.listBox.Location = new System.Drawing.Point(428, 40);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(329, 355);
+            this.listBox.TabIndex = 1;
+            // 
+            // buttonMover
+            // 
+            this.buttonMover.Location = new System.Drawing.Point(348, 200);
+            this.buttonMover.Name = "buttonMover";
+            this.buttonMover.Size = new System.Drawing.Size(60, 28);
+            this.buttonMover.TabIndex = 2;
+            this.buttonMover.Text = "Mover";
+            this.buttonMover.UseVisualStyleBackColor = true;
+            this.buttonMover.Click += new System.EventHandler(this.buttonMover_Click);
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonMover);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.checkedListBox);
+            this.Name = "Formulario";
+            this.Text = "CheckedListBox y ListBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button buttonMover;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+            //Con el método Add agregamos un ultimo Ítem a la
+            //Colección de Ítems. Como parámetro la cadena de caracteres.
+            checkedListBox.Items.Add("Diez");
+
+        }
+
+        private void buttonMover_Click(object sender, EventArgs e)
+        {
+            //Pregunta si la cantidad (Count) de elementos chequeados
+            //en la colección de ítems chequeados(CheckedItems) es mayor a cero.
+            if (checkedListBox.CheckedItems.Count > 0)
+            {
+                //Si es mayor a cero. Limpia los Ítem en el ListBox.
+                listBox.Items.Clear();
+
+                //La sentencia foreach recorre la colección de Ítems Seleccionados
+                //Y los asigna de a uno a la variable ítem del tipo String.
+                foreach (string item in checkedListBox.CheckedItems)
+                {
+                    //Agrego los ítems seleccionados en la colección
+                    //Al Listbox con el Método Add.
+                    listBox.Items.Add(item.ToString());
+                }
+                //Recorro todos los Ítems del CheckedListBox.
+                for (int i = 0; i < checkedListBox.Items.Count; i++)
+                    //Con el Método SetItemChecked, establezco en falso la
+                    //casilla de verificación (No esta seleccionado).
+                    //Como parámetros i-El índice y el valor de estado en este caso false.
+                    checkedListBox.SetItemChecked(i, false);
+
+            }
+        }
+    }
+}
